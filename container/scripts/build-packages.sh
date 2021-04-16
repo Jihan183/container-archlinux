aa66bb81d92dd1942415330a94ba960b9444db9c
smac89 <nobleman.code@gmx.com>
2021-04-16 14:02:24 -0600

    improving output

--- container/scripts/build-packages.sh
+++ container/scripts/build-packages.sh
@@ -5,14 +5,20 @@ source "${CONTAINER_BASE}/scripts/common.sh"
 
 cd "${XFCE_WORK_DIR}"
 
-echo -e "\n\Log output will be written to /tmp/build-packages\n\n"
+if [ -n "${ACTIONS_CI}" ]; then
+    log="$(mktemp /tmp/build-log.XXXXXXXXXXXX)" && {
+        echo "::set-output name=build_log::$log"
+    }
+fi
+
 {
+    echo -en "${log:+\n\nLog output will be written to $log\n\n}"
     exec {res}>&1
     runuser -- aur build \
         --ignorearch \
         --arg-file "${CONTAINER_BASE}/pkglist.txt" \
         --results /proc/$$/fd/${res} \
         --pkgver --database=custom \
-        --margs --syncdeps --noconfirm  >/tmp/build-packages
+        --margs --syncdeps --noconfirm >"${log:-/dev/fd/1}"
     exec >&${res}-
 } 2>&1
