e72e3d46e2a2e50194aa9b8e03cab772e59f2aca
smac89 <nobleman.code@gmx.com>
2021-04-18 16:09:58 -0600

    the output is still too verbose

--- container/scripts/build-packages.sh
+++ container/scripts/build-packages.sh
@@ -13,13 +13,11 @@ echo -en "${log:+\nLog output will be written to: $log\n}"
 
 result_pipe="/tmp/result_pipe"
 runuser -- mkfifo --mode=600 "$result_pipe"
-{
-    runuser -- aur build \
-        --ignorearch \
-        --arg-file "${CONTAINER_BASE}/pkglist.txt" \
-        --results "$result_pipe" --remove \
-        --pkgver --database=custom \
-        --margs --syncdeps --noconfirm >"${log:-/dev/fd/1}"
-} 2>&1 &
+runuser -- aur build \
+    --ignorearch \
+    --arg-file "${CONTAINER_BASE}/pkglist.txt" \
+    --results "$result_pipe" --remove \
+    --pkgver --database=custom \
+    --margs --syncdeps --noconfirm 2>&1 >"${log:-/dev/fd/1}" &
 
 stdbuf -oL tail --pid="$!" --follow --silent --bytes=+1 /proc/$!/fd/1 - <"$result_pipe"
