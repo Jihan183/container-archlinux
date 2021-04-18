af5d644ff50dfaa16dd40c5e7269191f172296c7
smac89 <nobleman.code@gmx.com>
2021-04-17 21:55:30 -0600

    use tail for logging

--- container/scripts/build-packages.sh
+++ container/scripts/build-packages.sh
@@ -11,16 +11,16 @@ if [ -n "${ACTIONS_CI}" ]; then
     echo "::set-output name=build_log::$log"
 fi
 
-echo -en "${log:+\n\nLog output will be written to $log\n\n}"
-{
-    result_pipe="$tmp/result_pipe"
-    runuser -- mkfifo --mode=600 "$result_pipe"
-    cat "$result_pipe" &
+echo -en "${log:+\nLog output will be written to: $log\n}"
 
-    runuser -- aur build \
-        --ignorearch \
-        --arg-file "${CONTAINER_BASE}/pkglist.txt" \
-        --results "$result_pipe" \
-        --pkgver --database=custom \
-        --margs --syncdeps --noconfirm > "${log:-/dev/fd/1}"
-} 2>&1
+result_pipe="$tmp/result_pipe"
+runuser -- mkfifo --mode=600 "$result_pipe"
+exec {aur}< <(runuser -- aur build \
+    --ignorearch \
+    --arg-file "${CONTAINER_BASE}/pkglist.txt" \
+    --results "$result_pipe" \
+    --pkgver --database=custom \
+    --margs --syncdeps --noconfirm 2>&1 >"${log:-/dev/fd/1}" &)
+
+stdbuf -oL tail --silent --bytes=+1 "$result_pipe" - <&${aur}
+exec {aur}<&-
