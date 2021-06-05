9301ac9582f160cdfb0373f3b1c8475745252ad9
smac89 <nobleman.code@gmx.com>
2021-06-05 03:18:24 -0600

    revert yay hack

--- container/scripts/pkg-utils.sh
+++ container/scripts/pkg-utils.sh
@@ -3,11 +3,8 @@
 # shellcheck source=container/scripts/common.sh
 source "${XFCE_BASE}/scripts/common.sh"
 
-# TODO: Restore yay-bin after it becomes compatible with pacman 6
-# https://aur.archlinux.org/packages/yay/#pinned-808720
-
 # hack to make sure yay-bin is used if the user picks yay
-# PACMAN_HELPER_URL="${PACMAN_HELPER_URL/%yay.git/yay-bin.git}"
+PACMAN_HELPER_URL="${PACMAN_HELPER_URL/%yay.git/yay-bin.git}"
 
 runuser -- git clone --depth=1 "${PACMAN_HELPER_URL}" /tmp/"${PACMAN_HELPER}"
 cd /tmp/"${PACMAN_HELPER}"
